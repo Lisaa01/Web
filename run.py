c74e6425cc2f6219ae32becd1dce986daf79c105
Lisaa <lizaveres000@gmail.com>
2023-02-27 17:45:32 +0100

    ROHADTSZAAAAR

---
+++ run.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for, redirect, render_template, request
+from data import users
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        users.append({'email': email, 'password': password})
+        return render_template("index.html")
+    return render_template("reg.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
