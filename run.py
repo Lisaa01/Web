2d06679ba15c6018db03a80b5ed9bddbfd27cd8e
Raistlin18 <126017684+Raistlin18@users.noreply.github.com>
2023-02-28 11:26:13 +0100

    Tessék, adatbáziska :)

--- run.py
+++ run.py
@@ -1,21 +1,61 @@
-from flask import Flask, url_for, redirect, render_template, request
-from data import users
+from flask import Flask, url_for, redirect, render_template, request, flash, session
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'hello'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column('id', db.Integer, primary_key=True)
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/view')
+def view():
+    return render_template('view.html', values=users.query.all())
+
+@app.route("/reg", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        email = request.form.get('email')
-        password = request.form.get('password')
-        users.append({'email': email, 'password': password})
-        return render_template("index.html")
-    return render_template("reg.html")
+        session.permanent = True
+        email = request.form['email']
+        session['email'] = email
+        found_email = users.query.filter_by(email=email).first()
+
+        if found_email:
+            session['password'] = found_email.password
+
+        else:
+            usr = users(email, '')
+            db.session.add(usr)
+            db.session.commit()
+
+        flash('User Saved!')
+        return redirect(url_for('index'))
+
+    else:
+        if 'email' in session:
+            flash('User alredy saved!')
+            return redirect(url_for('index'))
+        return render_template("reg.html")
 # Tesztet tesztelni jó!
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
